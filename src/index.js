e55b218ce808eb34c147c14f0e3647ed845d0309
Gabe Meola <gmeola@overstock.com>
2017-12-01 10:35:38 -0700

    changed export name

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 export { default as Provider } from './Provider';
 export { default as connect } from './connect';
 export { default as createStore } from './createStore';
-export { default as Predux } from './Predux';
+export { default as Aktie } from './Aktie';
 export { default as Module } from './Module';
