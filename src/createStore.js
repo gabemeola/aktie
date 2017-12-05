c990622fdffd35ebc797c3ce9add187f926f000a
Gabe Meola <gmeola@overstock.com>
2017-12-04 20:42:11 -0700

    rename

--- src/createStore.js
+++ src/createStore.js
@@ -1,13 +1,13 @@
-import Predux from './Predux';
+import Aktie from './Aktie';
 
 
 /**
- * Predux createion implementation wrapper for
- * creating a new Predux store.
+ * Aktie createion implementation wrapper for
+ * creating a new Aktie store.
  *
  * @param {Object} [hydrateState] - Object for modules to hydrate with
- * @return {Predux} - Returns Predux Instance
+ * @return {Aktie} - Returns Aktie Instance
  */
 export default function createStore(hydrateState) {
-    return new Predux(hydrateState);
+    return new Aktie(hydrateState);
 }
