0255f2807d86714159fc6b2a3855bee18d1c2e6f
Gabe Meola <gmeola@overstock.com>
2017-12-04 23:19:39 -0700

    added babel

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const pkg = require('./package.json');
+import babel from 'rollup-plugin-babel';
+
+
+const external = Object.keys(pkg.dependencies);
+
+module.exports = {
+    input: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        format: 'umd',
+        file: path.join(__dirname, 'lib', 'index.js'),
+    },
+    external,
+    name: 'aktie',
+    plugins: [
+        babel({
+            exclude: 'node_modules/**'
+        }),
+    ],
+};
